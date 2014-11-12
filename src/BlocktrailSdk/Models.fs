5838fb4ea6a6a18c09281444573640faad9ced0e
Raymen Scholten <raymenscholten@outlook.com>
2014-11-12 02:29:47 +0100

    Removed unused models

--- src/BlocktrailSdk/Models.fs
+++ src/BlocktrailSdk/Models.fs
@@ -3,6 +3,10 @@
 module Models =
 
     open System
+    open BlocktrailSdk.Helpers
+    open System.Collections.Generic
+    open System.Collections.ObjectModel
+    open Newtonsoft.Json
 
     type TransactionInput = 
         { index : int
@@ -25,58 +29,6 @@ module Models =
           spent_hash : string
           spent_index : int }
 
-    type Transaction = 
-        { hash : string
-          first_seen_at : string
-          last_seen_at : string
-          block_height : int
-          block_time : string
-          block_hash : string
-          confirmations : int
-          is_coinbase : bool
-          estimated_value : int64
-          total_input_value : int64
-          total_output_value : int64
-          total_fee : int
-          estimated_change : Nullable<int64>
-          estimated_change_address : string
-          high_priority : bool
-          enough_fee : bool
-          contains_dust : bool
-          inputs : TransactionInput array
-          outputs : TransactionOutput array }
-
-    type Address = 
-        { address : string
-          hash160 : string
-          balance : int
-          received : int
-          sent : int
-          unconfirmed_received : int
-          unconfirmed_sent : int
-          unconfirmed_transactions : int
-          total_transactions_in : int
-          total_transactions_out : int
-          category : string
-          tag : string }
-
-    type Block = 
-        { hash : string
-          height : int
-          block_time : string
-          difficulty : int64
-          merkleroot : string
-          is_orphan : bool
-          prev_block : string
-          next_block : string
-          byte_size : int
-          confirmations : int
-          transactions : int
-          value : int64
-          miningpool_name : string
-          miningpool_url : string
-          miningpool_slug : string }
-
     type Output = 
         { hash : string
           height : int
@@ -98,17 +50,6 @@ module Models =
           total : int64
           data : array<'T> }
 
-    type Request<'T> = 
-        { url : string
-          page : int
-          limit : int
-          sort_dir : string }
-
-
-    open BlocktrailSdk.Helpers
-    open System.Collections.Generic
-    open System.Collections.ObjectModel
-    open Newtonsoft.Json
 
     type PagingResponse<'T>(id, sortDir, data : Paging<'T>, reqfunc) =
         inherit ReadOnlyCollection<'T>(data.data)
